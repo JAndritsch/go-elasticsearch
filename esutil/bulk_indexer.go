5b66c0dff5d20f78337982fd2f73990ad2fce8fe
Joel Andritsch <joel.andritsch@gmail.com>
2025-02-15 07:46:34 -0600

    WIP: more debugging

--- esutil/bulk_indexer.go
+++ esutil/bulk_indexer.go
@@ -417,6 +417,7 @@ func (w *worker) run() {
 			w.bi.config.DebugLogger.Printf("[worker-%03d] Started\n", w.id)
 		}
 		defer func() {
+			fmt.Println("WORKER CLEANUP TRIGGERED")
 			w.flush(ctx)
 			w.ticker.Stop()
 			w.bi.wg.Done()
